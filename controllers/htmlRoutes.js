a1da6c40d8fdfe855cfd0a155de9aad375886e5b
amritank <amrita.sasi.nair@gmail.com>
2024-08-26 16:41:29 -0700

    reverting the stripe code changes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -173,59 +173,5 @@ router.get("/login", (req, res) => {
     res.render("login");
 });
 
-router.post("/webhook", async (req, res) => {
-    const event = req.body;
-    console.log("Amrita Event received: ", event);
-    console.log(event.type);
-    // Handle the event
-    switch (event.type) {
-        case 'checkout.session.completed':
-            console.log("Amrita -- checkout session received");
-            const cpaymentIntent = event.data.object;
-            const ccharityName = cpaymentIntent.metadata.charity;
-            console.log("AMRITA Charity name: " + ccharityName);
-            if (ccharityName === "X") {
-                console.log("AMRITA Updating charity X's wallet");
-            } else if (ccharityName === "Y") {
-                console.log("AMRITA Updating charity Y's wallet");
-            } else {
-                console.log("unknown");
-            }
-            console.log("Amrita -- checkout session completed end");
-
-            // Then define and call a method to handle the successful payment intent.
-            // handlePaymentIntentSucceeded(paymentIntent);
-            break;
-        case 'payment_intent.succeeded':
-            const paymentIntent = event.data.object;
-            const charityName = paymentIntent.metadata.charity;
-            console.log("AMRITA Charity name: " + charityName);
-            if (charityName === "X") {
-                console.log("AMRITA Updating charity X's wallet");
-            } else if (charityName === "Y") {
-                console.log("AMRITA Updating charity Y's wallet");
-            } else {
-                console.log("unknown");
-            }
-            console.log("Amrita -- payment received.")
-
-            // Then define and call a method to handle the successful payment intent.
-            // handlePaymentIntentSucceeded(paymentIntent);
-            break;
-        case 'payment_method.attached':
-            const paymentMethod = event.data.object;
-            console.log("Amrita -- payment method attached.")
-            // Then define and call a method to handle the successful attachment of a PaymentMethod.
-            // handlePaymentMethodAttached(paymentMethod);
-            break;
-        // ... handle other event types
-        default:
-            console.log(`Unhandled event type ${event.type}`);
-    }
-
-    // Return a response to acknowledge receipt of the event
-    res.json({ received: true });
-
-});
 
 module.exports = router;
